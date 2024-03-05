11bd31bb03034729d2101b0c490bb428152f23e9
winfredo <winfrednukpe2002@gmail.com>
2024-03-05 01:58:06 +0000

    Update

--- app/art/details/page.tsx
+++ app/art/details/page.tsx
@@ -3,15 +3,16 @@ import Header from "@/components/Header";
 import React from "react";
 import { BsCart4 } from "react-icons/bs";
 
-const ArtDetails = () => {
+    
+const ArtDetails: React.FC = () => {  
   return (
     <main>
       <Header />
 
       <Container>
-        <div className="pt-[10rem]  mx-auto">
+        <div className="pt-[10rem]">
           {/* main div for the two */}
-          <div className="flex	">
+          <div className="flex justify-evenly">
             <div>
               <img
                 src="/images/mainpicture.png"
@@ -53,17 +54,47 @@ const ArtDetails = () => {
                   <p className="text-[25px] font-medium">$1500</p>
 
 
-                  <button className="bg-black w-[100px] h-[35px] text-white rounded-[7px] flex justify-center items-center">
-                    <BsCart4  className=" mr-2"/>
-                    <p className="font-semibold text-[13px]">Add to cart</p>
+                  <button className="bg-black w-[110px] h-[35px] text-white rounded-[7px]  flex justify-center items-center">
+                    <BsCart4  className="pl- mr-2"/>
+                    <p className="font-semibold text-[12px]">Add to cart</p>
                   </button>
                 </div>
               </div>
               {/* beneath ends here */}
             </div>
           </div>
+
+          <div className="flex ml-[83px] h-[120px] w-[200px] mt-10 " >
+            <img src="../images/smallpic.png" className="mr-7"/>
+            <img src="../images/smallpic1.png" />
+          </div>
+
+          {/* Text underneath */}
+          <div className="ml-[83px]  w-[500px]">
+            <h2 className="mt-[4rem] mb-3 text-[23px] font-semibold">History</h2>
+            <p className="text-[13px]">
+            This set of 2 miniature ballet paintings, created in oil on canvas, features textured paint strokes and gentle shades. The paintings depict gracious ballerinas in white and black tutu dresses.
+            </p>
+            <p className="text-[13px]">
+            You can choose framed (wooden frame) or unframed (on canvas board) options.
+
+            </p>
+            <p className="text-[13px]">
+I use the highest quality European-made materials. Clear, gloss coating protects your cherished fine art investment from UV light, moisture, and dust. Canvas is 100% linen.
+            </p>
+            <p className="text-[13px] mb-[4rem]">
+            This set of miniature paintings is signed and dated by the artist, a certificate of authenticity is included.
+            </p>
+          </div>
+          <div className="border-t">
+          <p className="mt-[2rem] text-center text-[22px] font-medium">You May Also Like</p>
+
+         
+          </div>
+          
         </div>
       </Container>
+      
     </main>
   );
 };
